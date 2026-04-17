dba311c2df577974773fa2c9dcd5d76db84b026a
Zero <ZERO_BROTHER12345@outlook.com>
2026-04-17 11:19:32 +0100

    fixing some errors in the code and handling data errors

--- parsing.py
+++ parsing.py
@@ -1,37 +1,52 @@
 from typing import List, Dict
 
 
-class ParsingError(BaseException):
+class ParsingError(Exception):
     pass
 
 
 class ParsingFile:
 
-    def parse(self, line: List) -> None:
-        for chr in line:
-            if chr == '#':
-                raise ParsingError("Finding '#' in invalid place")
+    def parse_line(self, line: str) -> None:
+        line = line.strip()
 
-        if line[0] == "nb_drones":
+        if not line or line.startswith('#'):
+            return
 
+        if ':' not in line:
+            raise ParsingError("Invalid data, data should hold ':'")
 
+        key, val = line.split(':', 1)
+        key = key.strip()
+        
+        if key.strip('#') in "nb_drones" and key.startswith('#'):
+            raise ParsingError("Invalid data")
+        
+        if key in ['hub', 'start_hub', 'end_hub']:
+            values = val.split('#', 1)[0].strip().split(None, 3)
+            if len(values) < 3 or len(values) > 4:
+                raise ParsingError("Invalid data")
 
-        elif line[0] == "hub":
-
-        elif line[0] == "connection":
-
-
+            if len(values) >= 3:
+                if not values[1].strip('-').isdigit() or not values[2].strip('-').isdigit():
+                    raise ParsingError("Invalid data")
+                
+        elif key == 'nb_drones':
+            values = val.split('#', 1)[0].strip().split()
+            if len(values) != 1:
+                raise ParsingError("Invalid data")
+            
+        elif key == 'connection':
+            values = val.split('#', 1)[0].strip().split(None, 2)
+            if len(values) < 1 or len(values) > 2:
+                raise ParsingError("Invalid data")
 
 if __name__ == "__main__":
-    parsing = ParsingFile()
+    parse = ParsingFile()
 
-    try:
-        with open("maps/challenger/01_the_impossible_dream.txt", 'r') as f:
+    with open("maps/challenger/01_the_impossible_dream.txt") as f:
+        try:
             for line in f:
-                striped: str = line.strip()
-                if striped and striped[0] != '#':
-                    key, val = line.split(':', 1)
-                    data = [key.strip(), val.strip()]
-                    print(data)
-    except ParsingError as e:
-        print("Error: e")
+                parse.parse_line(line)
+        except Exception as e:
+            print(f"Error: {e}")

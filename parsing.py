79bee8bbeccb7c6a16733bde60ae8d4fd6ced372
Zero <ZERO_BROTHER12345@outlook.com>
2026-04-19 10:09:45 +0100

    cheking numbers is it a digit numbers and handling how many nb_drones, start_hub and end_hub

--- parsing.py
+++ parsing.py
@@ -6,8 +6,15 @@ class ParsingError(Exception):
 
 
 class ParsingFile:
+    
+    def __init__(self, count_nb_drones: int, count_nb_start_hub: int,
+                count_nb_end_hub: int) -> None:
+        self.count_nb_drones = count_nb_drones
+        self.count_nb_start_hub = count_nb_start_hub
+        self.count_nb_end_hub = count_nb_end_hub
 
     def parse_line(self, line: str) -> None:
+
         line = line.strip()
 
         if not line or line.startswith('#'):
@@ -19,30 +26,42 @@ class ParsingFile:
         key, val = line.split(':', 1)
         key = key.strip()
         
-        if key.strip('#') in "nb_drones" and key.startswith('#'):
-            raise ParsingError("Invalid data")
-        
         if key in ['hub', 'start_hub', 'end_hub']:
             values = val.split('#', 1)[0].strip().split(None, 3)
             if len(values) < 3 or len(values) > 4:
-                raise ParsingError("Invalid data")
+                raise ParsingError(f"Invalid {key}")
+            elif key == 'start_hub':
+                self.count_nb_start_hub += 1
+            elif key == 'end_hub':
+                self.count_nb_end_hub += 1
 
             if len(values) >= 3:
                 if not values[1].strip('-').isdigit() or not values[2].strip('-').isdigit():
-                    raise ParsingError("Invalid data")
+                    raise ParsingError("Invalid data, should be hold a digit number")
                 
         elif key == 'nb_drones':
             values = val.split('#', 1)[0].strip().split()
             if len(values) != 1:
-                raise ParsingError("Invalid data")
+                raise ParsingError("Invalid nb_drones data")
+            self.count_nb_drones += 1
             
         elif key == 'connection':
             values = val.split('#', 1)[0].strip().split(None, 2)
             if len(values) < 1 or len(values) > 2:
-                raise ParsingError("Invalid data")
+                raise ParsingError("Invalid connection data")
+            
+        if self.count_nb_drones > 1:
+            raise ParsingError("too many nb_drones")
+        if self.count_nb_end_hub > 1:
+            raise ParsingError("too many nb_end_hub")
+        if self.count_nb_start_hub > 1:
+            raise ParsingError("too many nb_start_hub")
+
+
 
 if __name__ == "__main__":
-    parse = ParsingFile()
+    parse = ParsingFile(count_nb_drones=0, count_nb_start_hub=0,
+                        count_nb_end_hub=0)
 
     with open("maps/challenger/01_the_impossible_dream.txt") as f:
         try:

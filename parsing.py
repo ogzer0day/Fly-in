5d03ac54f7417c980f43b3b1e78ad7fc17f3c091
Zero <ZERO_BROTHER12345@outlook.com>
2026-04-20 11:53:40 +0100

    creating The  methode that  will be responsible for extracting all the details of an individual hub (node) from a single line in the map file

--- parsing.py
+++ parsing.py
@@ -8,10 +8,13 @@ class ParsingError(Exception):
 class ParsingFile:
     
     def __init__(self, count_nb_drones: int, count_nb_start_hub: int,
-                count_nb_end_hub: int) -> None:
+                count_nb_end_hub: int, hub_data: dict,
+                connection_data: dict) -> None:
         self.count_nb_drones = count_nb_drones
         self.count_nb_start_hub = count_nb_start_hub
         self.count_nb_end_hub = count_nb_end_hub
+        self.hub_data = hub_data
+        self.connection_data = connection_data
 
     def parse_line(self, line: str) -> None:
 
@@ -56,7 +59,7 @@ class ParsingFile:
         line = line.strip()
 
         if not line or line.startswith('#'):
-            return
+            returncolor==red
 
         if self.count_nb_drones != 1:
             raise ParsingError("Data most hold 1 nb_drones")
@@ -76,12 +79,78 @@ class ParsingFile:
             else:
                 return (nb)
 
-    def parse_hub(line: str, is_start: bool, is_end:bool) -> Dict:
-        pass
+
+    # def parse_properties(self, properties_string: str, hub_type: str) -> None:
+    #     properties_string = properties_string.split('[', 1)[1].strip()
+    #     properties_string = properties_string.split(']', 1)[0].strip()
+    #     properties_string = properties_string.split(None, 2)
+    #     properties_string = properties_string.split(' ', 1)[0]
+
+    #     for val in properties_string:
+    #         val = val.split('=', 1)
+    #         if val[1][0] == '=':
+    #             raise ParsingError("Invalid properties have more than one '='")
+    #         else:
+    #             if val[0] == 'color':
+    #                 hub_data[hub_type]['properties']['color'] = val[1]
+    #             elif val[0] == 'zone':
+    #                 hub_data[hub_type]['properties']['zone'] = val[1]
+    #             elif val[0] == 'max_drones':
+    #                 hub_data[hub_type]['properties']['max_drones'] = val[1]
+
+
+    def parse_hub(self, line: str, is_start: bool, is_end:bool) -> Dict:
+        line = line.strip()
+
+        if not line or line.startswith('#'):
+            return
+        
+        if is_start and self.count_nb_start_hub != 1:
+            raise ParsingError("Data most hold 1 start_hub")
+        if is_end and self.count_nb_end_hub!= 1:
+            raise ParsingError("Data most hold 1 end_hub")
+
+        key, val = line.split(':', 1)
+        key = key.strip()
+        val = val.strip()
+        val = val.split('#', 1)[0].strip().split(None, 3)
+
+        name = val[0]
+        if is_start and name not in 'start_hub':
+            raise ParsingError("Invalid 'start_hub' name")
+        elif is_end and name not in 'end_hub':
+            raise ParsingError("Invalid 'end_hub' name")
+        elif not is_start and not is_end and name not in 'hub':
+            raise ParsingError("Invalid 'hub' name")
+        try:
+            x = int(val[1])
+            y = int(val[2])
+        except ValueError:
+            raise ParsingError("The value 'x, y' in hub most be an int")
+        
+
+        if is_start:
+            self.hub_data['start'] = {'name': name, 'X': x, 'Y': y, 'properties': {'color': None, 'zone': 'normal', 'max_drones': 1}}
+        elif is_end:
+            self.hub_data['end'] = {'name': name, 'X': x, 'Y': y, 'properties': {'color': None, 'zone': 'normal', 'max_drones': 1}}
+        else:
+            self.hub_data['hub'] = {'name': name, 'X': x, 'Y': y, 'properties': {'color': None, 'zone': 'normal', 'max_drones': 1}}
+
+        if len(val) == 4:
+            properties = val[3]
+            if is_start:
+                parse_properties(properties, 'start')
+            elif is_end:
+                parse_properties(properties, 'end')
+            else:
+                parse_properties(properties, 'hub')
+
+        print(self.hub_data)
+
 
 if __name__ == "__main__":
     parse = ParsingFile(count_nb_drones=0, count_nb_start_hub=0,
-                        count_nb_end_hub=0)
+                        count_nb_end_hub=0, hub_data={}, connection_data={})
 
     with open("maps/challenger/01_the_impossible_dream.txt") as f:
         try:
